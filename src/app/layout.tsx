12bfeded3744cc9a4af0b195947a470b15185487
Kenost <Kenostru@mail.ru>
2024-05-30 22:09:33 +0500

    push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import 'primeicons/primeicons.css';
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 import "./globals.css";
+import 'rsuite/dist/rsuite-no-reset.min.css';
+import { CustomProvider } from 'rsuite';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +20,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <PrimeReactProvider>
+      <CustomProvider>
+        <html lang="en">
+          <body className={inter.className}>
+            {children}</body>
+        </html>
+      </CustomProvider>
+    </PrimeReactProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,95 @@
+'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Container, Header, Content, Footer, Navbar, Nav } from 'rsuite';
+import CogIcon from '@rsuite/icons/legacy/Cog';
+import { Chart } from 'primereact/chart';
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 
 export default function Home() {
+
+  const [statistics, setStatistics] = useState<any>([]);
+
+  useEffect(() => {
+    axios.get(`/api/statistics`).then((res) => {
+          if(res.data.success){
+              setStatistics(res.data.data);
+          }
+      });
+  }, [])
+
+  const data = {
+    labels: ['Пунктов выдачи заказов', 'Складов', 'Транзитных городов'],
+    datasets: [
+        {
+            data: [statistics?.city?.PickPoint, statistics?.city?.Warehouse, statistics?.city?.Transit],
+            backgroundColor: [
+                "#3b82f6", "#eab308", "#22c55e"
+            ],
+            hoverBackgroundColor: [
+                "#3b82f6", "#eab308", "#22c55e"
+            ]
+        }
+    ]
+  };
+  const options = {
+      cutout: '60%',
+      plugins: false
+  };
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="show-fake-browser navbar-page">
+        <Container>
+          <Header>
+            <Navbar appearance="inverse">
+              <Navbar.Brand>
+                <a style={{ color: '#fff' }}><img className="logo" src="/logo.svg"/></a>
+              </Navbar.Brand>
+              {/* <Nav>
+                <Nav.Item>Home</Nav.Item>
+                <Nav.Item>News</Nav.Item>
+                <Nav.Item>Products</Nav.Item>
+                <Nav.Menu title="About">
+                  <Nav.Item>Company</Nav.Item>
+                  <Nav.Item>Team</Nav.Item>
+                  <Nav.Item>Contact</Nav.Item>
+                </Nav.Menu>
+              </Nav> */}
+              <Nav pullRight>
+                <Nav.Item icon={<CogIcon />}>Settings</Nav.Item>
+              </Nav>
+            </Navbar>
+          </Header>
+          <Content className="content-main">
+            <h1>Активные приборы:</h1>
+            <div className="electr">
+              <img src="/lamp.png" alt="" />
+              <div className="info">
+                <b>Лампа</b>
+                <p>Среднее потреблоение: </p>
+              </div>
+              <Chart style={{width: "100px"}} type="doughnut" data={data} options={options} />
+            </div>
+            <div className="electr">
+              <img src="/toaster.webp" alt="" />
+              <div className="info">
+                <b>Тостер</b>
+                <p>Среднее потреблоение: </p>
+              </div>
+            </div>
+            <div className="electr">
+              <img src="/wentel.webp" alt="" />
+              <div className="info">
+                <b>Вентилятор</b>
+                <p>Среднее потреблоение: </p>
+              </div>
+            </div>
+          </Content>
+          <Footer></Footer>
+        </Container>
       </div>
     </main>
   );
